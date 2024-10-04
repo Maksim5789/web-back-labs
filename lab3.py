4f674779aa6788b91aaee1da0f719545f7bb0e43
Акишин Максим <maksim.akishin.04@mail.ru>
2024-10-04 20:28:41 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -34,3 +34,32 @@ def form1():
         errors['age'] = "Заполните поле!"
     sex = request.args.get('sex')
     return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    # Пусть кофе стоит - 120 рублей, чёрный чай - 80 рублей, зелёный чай - 70 рублей.
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    # Добавка молока удорожает напиток на 30 рублей, а сахара - на 10
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+    
+    return render_template('pay.html', price=price)
+
+@lab3.route('/lab3/success')
+def success():
+    price = request.args.get('price')
+    return render_template('success.html', price=price)
