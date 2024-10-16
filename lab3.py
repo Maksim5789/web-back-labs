4a30477aa32306a066a45076ed08d08f82941fa8
Акишин Максим <student@FB.LOCAL>
2024-10-16 18:37:36 +0700

    Задания

--- lab3.py
+++ lab3.py
@@ -11,7 +11,7 @@ def lab():
 @lab3.route('/lab3/cookie')
 def cookie():
     resp = make_response(redirect('/lab3/'))
-    resp.set_cookie('name', 'Alex', max_age=5)
+    resp.set_cookie('name', 'Максим', max_age=5)
     resp.set_cookie('age', '20')
     resp.set_cookie('name_color', 'magenta')
     return resp
