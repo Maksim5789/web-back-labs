5a3e5ab09349b2e0366ee3304b50341bbe701312
Акишин Максим <maksim.akishin.04@mail.ru>
2024-10-27 17:56:07 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -22,4 +22,90 @@ def div():
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
 
+@lab4.route('/lab4/add-form')
+def add_form():
+    return render_template('lab4/add-form.html')
+
+@lab4.route('/lab4/add', methods=['POST'])
+def add():
+    x1 = request.form.get('x1')  
+    x2 = request.form.get('x2')  
+    # Преобразуем в 0, если пустая строка
+    if x1 == '':
+        x1 = 0
+    else:
+        x1 = int(x1)
+    
+    if x2 == '':
+        x2 = 0
+    else:
+        x2 = int(x2)
+
+    result = x1 + x2
+    return render_template('lab4/add.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/multiply-form')
+def multiply_form():
+    return render_template('lab4/multiply-form.html')
+
+@lab4.route('/lab4/multiply', methods=['POST'])
+def multiply():
+    x1 = request.form.get('x1')  
+    x2 = request.form.get('x2')  
+
+    # Преобразуем в 1, если пустая строка
+    if x1 == '':
+        x1 = 1
+    else:
+        x1 = int(x1)
+    
+    if x2 == '':
+        x2 = 1
+    else:
+        x2 = int(x2)
+
+    result = x1 * x2
+    return render_template('lab4/multiply.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/subtract-form')
+def subtract_form():
+    return render_template('lab4/subtract-form.html')
+
+@lab4.route('/lab4/subtract', methods=['POST'])
+def subtract():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    if x1 == '' or x2 == '':
+        return render_template('lab4/subtract.html', error='Оба поля должны быть заполнены!')
+
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 - x2
+    return render_template('lab4/subtract.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/power-form')
+def power_form():
+    return render_template('lab4/power-form.html')
+
+@lab4.route('/lab4/power', methods=['POST'])
+def power():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+
+    if x1 == '' or x2 == '':
+        return render_template('lab4/power.html', error='Оба поля должны быть заполнены!')
+
+    x1 = int(x1)
+    x2 = int(x2)
+
+    if x1 == 0 and x2 == 0:
+        return render_template('lab4/power.html', error='0 в степени 0 определено как неопределенность!')
+
+    result = x1 ** x2
+    return render_template('lab4/power.html', x1=x1, x2=x2, result=result)
+
+
+
 
