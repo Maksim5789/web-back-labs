66f6fdbb71dce79df825e4913a4044718694161f
Акишин Максим <maksim.akishin.04@mail.ru>
2024-12-10 21:30:06 +0700

    Получение информации о фильме/-ах по REST

--- lab7.py
+++ lab7.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, render_template, request, session, current_app, jsonify, flash
+from flask import Blueprint, redirect, url_for, render_template, request, session, current_app, jsonify, flash, abort
 import sqlite3
 from os import path
 import hashlib
@@ -8,3 +8,35 @@ lab7 = Blueprint('lab7', __name__)
 @lab7.route('/lab7/')
 def main():
     return render_template('lab7/lab7.html')
+
+films = [
+    {
+        "title": "Inception",
+        "title_ru": "Начало",
+        "year": 2010,
+        "description": "A skilled thief is given a final chance to redeem himself by performing an impossible heist: infiltrating dreams to steal valuable secrets."
+    },
+    {
+        "title": "The Shawshank Redemption",
+        "title_ru": "Побег из Шоушенка",
+        "year": 1994,
+        "description": "Two imprisoned men bond over several years, finding solace and eventual redemption through acts of common decency."
+    },
+    {
+        "title": "The Dark Knight",
+        "title_ru": "Темный рыцарь",
+        "year": 2008,
+        "description": "Batman sets out to dismantle the remaining criminal organizations that plague Gotham, facing the formidable Joker who seeks to undermine Batman's influence."
+    },
+]
+
+@lab7.route('/lab7/rest-api/films/', methods=['GET'])
+def get_films():
+    return films
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
+def get_film(id):
+    # Проверка на корректность id
+    if id < 0 or id >= len(films):
+        abort(404)  # Возвращаем ошибку 404, если id выходит за пределы
+    return films[id]
