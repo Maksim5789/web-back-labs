afb15e58113f22ad8270278e3ff1deabdadc1f31
Акишин Максим <maksim.akishin.04@mail.ru>
2024-12-10 23:02:31 +0700

    Удаление фильма

--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
 function fillFilmList() {
     fetch('/lab7/rest-api/films/')
-    .then(function (data) {
-        return data.json();
+    .then(function (response) {
+        return response.json();
     })
     .then(function (films) {
         let tbody = document.getElementById('film-list');
@@ -13,26 +13,46 @@ function fillFilmList() {
             let tdTitleRus = document.createElement('td');
             let tdYear = document.createElement('td');
             let tdActions = document.createElement('td');
-
+        
             tdTitle.innerText = films[i].title == films[i].title_ru ? '' : films[i].title;
             tdTitleRus.innerText = films[i].title_ru;
             tdYear.innerText = films[i].year;
-
+        
             let editButton = document.createElement('button');
             editButton.innerText = 'Редактировать';
-
+        
             let delButton = document.createElement('button');
             delButton.innerText = 'Удалить';
-
+            delButton.onclick = function() {
+                deleteFilm(i, films[i].title_ru); // Исправлено: передаем индекс i вместо films[i].id
+            };
+        
             tdActions.append(editButton);
             tdActions.append(delButton);
-    
+        
             tr.append(tdTitle);
             tr.append(tdTitleRus);
             tr.append(tdYear);
             tr.append(tdActions);
-    
+        
             tbody.append(tr);
         }
+    });
+}
+
+function deleteFilm(id, title) {
+    if(! confirm(`Вы точно хотите удалить фильм "${title}"?`))
+        return;
+    fetch(`/lab7/rest-api/films/${id}`, {method: 'DELETE'})
+    .then(function (response) {
+        if (response.status === 204) {
+            fillFilmList(); // Обновляем список фильмов
+        } else {
+            alert('Ошибка при удалении фильма');
+        }
+    })
+    .catch(function (error) {
+        console.error('Ошибка при удалении фильма:', error);
+        alert('Не удалось удалить фильм. Пожалуйста, попробуйте позже.');
     });
 }
