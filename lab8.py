231a15361b9fbb03ab6b831353639c0f23d9e29d
Акишин Максим <maksim.akishin.04@mail.ru>
2024-12-20 10:31:47 +0700

    Добавление галочки с запоминанием и кодировки utf-8 для секретного ключа, чтобы избежать ошибки с latin-1

--- lab8.py
+++ lab8.py
@@ -39,18 +39,20 @@ def login():
     
     login_form = request.form.get('login')
     password_form = request.form.get('password')
+    remember = request.form.get('remember')  # Получаем значение галочки
 
     if not (login_form and password_form):
         return render_template('lab8/login.html', error='Заполните поля')
     
-    user = users.query.filter_by(login = login_form).first()
+    user = users.query.filter_by(login=login_form).first()
 
     if user:
         if check_password_hash(user.password, password_form):
-            login_user(user, remember = False)
+            login_user(user, remember=bool(remember))  # Устанавливаем remember
             return redirect('/lab8/')
     
-    return render_template('/lab8/login.html', error = 'Ошибка входа: логин и/или пароль неверны')
+    return render_template('/lab8/login.html', error='Ошибка входа: логин и/или пароль неверны')
+
 
 @lab8.route('/lab8/register', methods=['GET', 'POST'])
 def register():
